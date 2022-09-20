08fdacc2a2080f2f1e9e90c281ddc9eb66cd8d7f
Vishakha Tuteja <vishakhatuteja@fico.com>
2022-09-21 00:01:32 +0530

    remove duplicate value of one

--- CraftDemo/src/com/BinaryTree/BinaryTree.java
+++ CraftDemo/src/com/BinaryTree/BinaryTree.java
@@ -36,42 +36,40 @@ public class BinaryTree {
 		root.left.right = new Node(5);
 		root.right.right = new Node(6);
 		root.right.right.left = new Node(7);
-		root.right.right.right = new Node(1);
 
-		Vector<Integer> rootsAfterDeletionOfAvailableNode = removeNode(root, 2);
-		System.out.println(rootsAfterDeletionOfAvailableNode);
-
+		Vector<Integer> remainingRoots = removeNode(root, 1);
+		System.out.println(remainingRoots);
 	}
 
 	private static Vector<Integer> removeNode(Node root, int nodeToBeRemoved) {
 		// TODO Auto-generated method stub
 		
-		Vector<Integer> forestRoots = new Vector<Integer>();
+		Vector<Integer> remainingRoots = new Vector<Integer>();
 		
-		if(removeNodeUtil(root, nodeToBeRemoved, forestRoots) != null) {
-			forestRoots.add(root.id);
+		if(getSubTreeRoot(root, nodeToBeRemoved, remainingRoots) != null) {
+			remainingRoots.add(root.id);
 		}
 		
-		return forestRoots;
-		
+		return remainingRoots;
 	}
 
-	private static Node removeNodeUtil(Node root, int nodeToBeRemoved, Vector<Integer> forestRoots) {
+	private static Node getSubTreeRoot(Node root, int nodeToBeRemoved, Vector<Integer> remainingRoots) {
 		// TODO Auto-generated method stub
+		
 		if(root == null) {
 			return null;
 		}
 		
-		root.left = removeNodeUtil(root.left, nodeToBeRemoved, forestRoots);
-		root.right = removeNodeUtil(root.right, nodeToBeRemoved, forestRoots);
+		root.left = getSubTreeRoot(root.left, nodeToBeRemoved, remainingRoots);
+		root.right = getSubTreeRoot(root.right, nodeToBeRemoved, remainingRoots);
 		
 		if(root.id == nodeToBeRemoved){
             if(root.left != null) {
-            	forestRoots.add(root.left.id);
+            	remainingRoots.add(root.left.id);
             }
             
             if(root.right != null) {
-            	forestRoots.add(root.right.id);
+            	remainingRoots.add(root.right.id);
             }
             
             return null;
